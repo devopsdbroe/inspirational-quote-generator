505ba780400211ec1715d8d5fd3c2c16160318e8
devopsdbroe <dbroe1995@gmail.com>
2024-01-22 20:59:23 -0500

    Add button with a lottie image

--- components/animations/AnimatedDownloadButton.tsx
+++ components/animations/AnimatedDownloadButton.tsx
@@ -1,7 +1,26 @@
 import Image from "next/image";
 
+import Lottie from "react-lottie-player";
+import lottieJson from "../../assets/animated-photo.json";
+import {
+	CenteredLottie,
+	DownloadQuoteCardContainer,
+	DownloadQuoteCardContainerText,
+} from "./AnimationElements";
+
 const AnimatedDownloadButton = () => {
-	return <div>AnimatedDownloadButton</div>;
+	return (
+		<DownloadQuoteCardContainer onClick={() => {}}>
+			<CenteredLottie
+				loop
+				animationData={lottieJson}
+				play
+			/>
+			<DownloadQuoteCardContainerText>
+				Download your quote card
+			</DownloadQuoteCardContainerText>
+		</DownloadQuoteCardContainer>
+	);
 };
 
 export default AnimatedDownloadButton;
